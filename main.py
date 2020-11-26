d96c32dd0e5128ecf86b570cda7fb3e1db13de3b
netresj <54282375+netresj@users.noreply.github.com>
2020-11-26 11:32:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ import numpy as np
 
 # 可視化に使用
 import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
 
-# データの読み込み
+# 学習に使う keras モジュール
 from tensorflow.keras.datasets.mnist import load_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
 
+# 精度評価などに使用するライブラリ
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 (X_train, y_train), (X_test, y_test) = load_data()
 
+fig_id = 0
 
 def data_visualize(image, label, predict=None):
     plt.figure(figsize=(5, 5))
@@ -20,8 +28,39 @@ def data_visualize(image, label, predict=None):
     plt.title(title)
     plt.imshow(image)
     plt.gray()
-    plt.savefig("/kqi/output/figure/fig.png")
-
+    plt.savefig("/kqi/output/figure/fig" + str(fig_id) + ".png")
+    fig_id += 1
 
 for i in range(3):
     data_visualize(X_train[i], y_train[i])
+
+X_train = X_train.astype("float32") / 255
+X_test = X_test.astype("float32") / 255
+
+y_train = to_categorical(y_train)
+
+def build_model():
+    model = Sequential()
+    model.add(Flatten())
+    model.add(Dense(200, activation="relu"))
+    model.add(Dense(200, activation="relu"))
+    model.add(Dense(10, activation="softmax"))
+    model.compile(optimizer="adam",
+                  loss="categorical_crossentropy")
+    return model
+
+model = build_model()
+
+model.fit(X_train, y_train)
+
+y_predict = model.predict(X_test)
+
+y_predict = np.argmax(y_predict, axis=1)
+
+cm = confusion_matrix(y_test, y_predict)
+sns.heatmap(cm, cmap='Blues', annot=True, square=True)
+plt.savefig("/kqi/output/figure/confution_matrix.png")
+for i in range(cm.shape[0]):
+    cm[i, i] = 0
+sns.heatmap(cm, cmap='Blues', annot=True, square=True)
+plt.savefig("/kqi/output/figure/confution_matrix_v2.png")
